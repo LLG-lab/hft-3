2d5a3bb0324790eaa85a2c07f1f7508e0fe23b40
Ryszard Gradowski <ryszard.gradowski@gmail.com>
2020-10-13 22:28:52 +0200

    Mods in additional files for rpm generation purposes, vol3

--- etc/deploy/hftwatchdog-run.sh
+++ etc/deploy/hftwatchdog-run.sh
@@ -1,21 +1,21 @@
 #!/bin/bash -e
 
-SWITCH_STATUS=$(< /var/lib/hft/hft-watchdog-switch)
+declare -r SWITCH_STATUS=$(< /var/lib/hft/hftwatchdog-run.status)
 
 if [ "x$SWITCH_STATUS" = "xenabled" ] ; then
     SERVER_RUNNING=$(ps aux | grep "hft server" | grep -v grep || true)
     if [ "x$SERVER_RUNNING" = "x" ] ; then
-        # Server zdechł.
+        # Server is dead.
         SMSMSG=$(< /var/lib/hft/hft-watchdog-lastrun)
         SMSMSG="hft server is dead, last runned ${SMSMSG}"
-        # Wysłanie sms-a.
+        # Sending SMS message..
         echo $SMSMSG
         /usr/bin/caans --client-message="${SMSMSG}" && echo "disabled" > /var/lib/hft/hft-watchdog-switch
      else
-        # Serwer żyje, zapisz date ostatniego sprawdzenia.
+        # Server is alive, save last check time stamp.
         LAST_CHECK=$(date)
         echo $LAST_CHECK > /var/lib/hft/hft-watchdog-lastrun
      fi
 else
-    echo "`date` : Watchdog wyłączony, nie ma co sprawdzać"
+    echo "`date` : Watchdog disabled, nothing to do."
 fi
